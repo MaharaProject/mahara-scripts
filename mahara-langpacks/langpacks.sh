3131a5504669ce011aa40be520aa4a940c50cff6
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-02-25 11:59:30 +1300

    Add more langs

--- mahara-langpacks/langpacks.sh
+++ mahara-langpacks/langpacks.sh
@@ -45,7 +45,7 @@ echo "Checking langpacks for updates: `date \"+%Y-%m-%d %H:%M:%S\"`"
 [ ! -d ${CLEAN} ] && mkdir ${CLEAN}
 [ ! -d ${TARBALLS} ] && mkdir ${TARBALLS}
 
-langs="ca cs de en_us es eu fi fr he it ja ko nl no_nb sl zh_tw"
+langs="ca cs da de en_us es eu fi fr he it ja ko nl no_nb ru_ru sl zh_tw"
 
 for lang in ${langs} ; do
 
