853dd978610cb54d7fa1103ffd68eea286d57292
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-05-09 15:44:47 +1200

    Stop cron scripts from spamming root with nasty emails
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

--- debian/custom-site-mahara-langpacks.cron.d
+++ debian/custom-site-mahara-langpacks.cron.d
@@ -1,6 +1,6 @@
 #
 # Regular cron jobs for the custom-site-mahara-langpacks package
 #
-22 *    * * *   root    [ -x /usr/lib/mahara-langpacks/langpacks.sh ] && /usr/lib/mahara-langpacks/langpacks.sh >> /var/log/mahara-langpacks.log
-52 *    * * *   root    [ -x /usr/lib/mahara-langpacks/update-pot.sh ] && /usr/lib/mahara-langpacks/update-pot.sh >> /var/log/mahara-langpacks.log
+22 *    * * *   root    [ -x /usr/lib/mahara-langpacks/langpacks.sh ] && /usr/lib/mahara-langpacks/langpacks.sh >> /var/log/mahara-langpacks.log 2>&1
+52 *    * * *   root    [ -x /usr/lib/mahara-langpacks/update-pot.sh ] && /usr/lib/mahara-langpacks/update-pot.sh >> /var/log/mahara-langpacks.log 2>&1
 
